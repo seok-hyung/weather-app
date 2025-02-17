1f86e89c46c8f39008957d5d6dae4cc50f7cdf5d
seok-hyung <korea970223@naver.com>
2025-02-17 09:57:34 +0900

    🐛 API url http -> https

--- src/hooks/use-weather.ts
+++ src/hooks/use-weather.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY
 const URL = 'https://api.openweathermap.org/data/3.0'
-const GEO_URL = 'http://api.openweathermap.org/geo/1.0'
+const GEO_URL = 'https://api.openweathermap.org/geo/1.0'
 
 const fetchAllWeather = async (lat: number, lon: number) => {
   const { data } = await axios.get(`${URL}/onecall`, {
